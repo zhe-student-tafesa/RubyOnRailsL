9227938a5faaa9cc10a9c8f456541ae564a799af
frankzhang2022 <frank.zhang@pixelforce.com.au>
2022-12-03 18:54:11 +1030

    5- use partial to reuse code: like nav and footer  [if end]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,20 @@
 class HomeController < ApplicationController
     def index
+        @nav_flag = false
     end
 
+    def first
+        @nav_flag = true
+    end
+    def second
+        @nav_flag = true
+    end
+
+
     def show_post
+
+        @nav_flag = true
+
         @testName = params[:id]
     end 
 end
